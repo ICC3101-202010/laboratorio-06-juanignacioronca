6da69a716a1066d3f8955c29eb848be10af4d124
juan ignacio roncagliolo <juanignacioroncagliolo@hotmail.com>
2020-05-16 11:38:25 -0400

    Agregar archivos de proyecto.

---
+++ Laboratorio 06/Empresa.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Laboratorio_06
+{
+    [Serializable]
+    class Empresa
+    {
+        public string Nombre;
+        public string RUT;
+
+        public Empresa(string nombre, string rUT)
+        {
+            Nombre = nombre;
+            RUT = rUT;
+        }
+    }
+}
---
+++ Laboratorio 06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido:\n1.Para crear una nueva empresa\n2Para leer empresas creadas previamente\n0 para salir.");
+            int a = -1;
+            while (a!=0)
+            {
+                if (a==1)
+                {
+
+                }
+            }
+        }
+    }
+}
